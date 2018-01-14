03cb4393a29df89ec2d89b67250a0a73c039c8ad
CruftStuffer <35177702+CruftStuffer@users.noreply.github.com>
2018-01-14 18:10:17 -0500

    title screeen + music

--- WindowsApp1/HomeScreen.vb
+++ WindowsApp1/HomeScreen.vb
@@ -4,10 +4,10 @@
     End Sub
 
     Private Sub btnCreate_Click(sender As Object, e As EventArgs) Handles btnCreate.Click
-        ObjPwr1.Show()
+        frmCreate.Show()
     End Sub
 
     Private Sub HomeScreen_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
+        My.Computer.Audio.Play(My.Resources._8_Bit_Mayhem_1_, AudioPlayMode.BackgroundLoop)
     End Sub
 End Class
